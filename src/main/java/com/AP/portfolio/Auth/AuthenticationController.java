4ae69463360b89ed4eaeda9ecdfa8bcda3140aeb
tomas maaggi <tm82945g@gmail.com>
2023-02-06 23:37:11 -0300

    disable controller to add new users

--- src/main/java/com/AP/portfolio/Auth/AuthenticationController.java
+++ src/main/java/com/AP/portfolio/Auth/AuthenticationController.java
@@ -14,12 +14,14 @@ public class AuthenticationController {
 
   private final AuthenticationService service;
 
-  @PostMapping("/register")
-  public ResponseEntity<AuthenticationResponse> register(
-      @RequestBody RegisterRequest request
-  ) {
-    return ResponseEntity.ok(service.register(request));
-  }
+//  this will be unavaliable for now
+//  
+//  @PostMapping("/register")
+//  public ResponseEntity<AuthenticationResponse> register(
+//      @RequestBody RegisterRequest request
+//  ) {
+//    return ResponseEntity.ok(service.register(request));
+//  }
   @PostMapping("/authenticate")
   public ResponseEntity<AuthenticationResponse> authenticate(
       @RequestBody AuthenticationRequest request
