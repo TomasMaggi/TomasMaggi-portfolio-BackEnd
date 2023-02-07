477b78f3e7753269d63bb8b4be25d2509d673b17
tomas maaggi <tm82945g@gmail.com>
2023-02-07 01:26:08 -0300

    fix cors and db conection problems

--- src/main/java/com/AP/portfolio/Configs/SecurityConfiguration.java
+++ src/main/java/com/AP/portfolio/Configs/SecurityConfiguration.java
@@ -25,16 +25,16 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .cors().configurationSource(request -> {
+        .cors()
+            .configurationSource(request -> {
               var cors = new CorsConfiguration();
-              cors.setAllowedOrigins(List.of("http://localhost:4200", "http://127.0.0.1:80", "http://example.com"));
+              cors.setAllowedOrigins(List.of("*"));
               cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
               cors.setAllowedHeaders(List.of("*"));
               return cors;
             })
         .and()
-        .csrf()
-        .and()
+        .csrf().disable()
         .authorizeHttpRequests()
             // auth request, no need of autorization
         .requestMatchers("/api/v1/auth/**")
